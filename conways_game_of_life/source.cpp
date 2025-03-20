3422045587c76bbc92ba5b74bf49c3083280f7b9
AnruKitakaze <42811808+AnruKitakaze@users.noreply.github.com>
2025-03-21 03:04:34 +0700

    add restored by Google Gemini cyrillic

--- conways_game_of_life/source.cpp
+++ conways_game_of_life/source.cpp
@@ -2,11 +2,11 @@
 #include <ctime>
 #include <Windows.h>
 
-#define CELL_SYMBOL 'Î' //Ñèìâîë, âûâîäèìûé íà ìåñòå æèâîé êëåòêè.
-#define INDENT      36 //Îòñòóï â 3 ñòðîêè äëÿ âûâîäà èíôîðìàöèè
+#define CELL_SYMBOL 'Î' //Символ, выводимый на месте живой клетки.
+#define INDENT      36 //Отступ в 3 строки для вывода информации
 using namespace std;
 
-//Øèðèíà è âûñîòà íà 2 åä. áîëüøå çàäàííûõ ïîëüçîâàòåëåì
+//Ширина и высота на 2 ед. больше заданных пользователем
 struct Settings {
 	int         fieldWidth;
 	int         fieldHeight;
@@ -14,20 +14,20 @@ struct Settings {
 	long long   maxStep;
 };
 
-//Äëÿ ïîëó÷åíèÿ ñâåäåíèé î íàñòðîéêàõ òåêóùåé èãðîâîé ñåññèè
+//Для получения сведений о настройках текущей игровой сессии
 void primarySetup(Settings&);
-//Âûâîä ïîëÿ íà ýêðàí êîíñîëè(WinAPI)
+//Вывод поля на экран консоли(WinAPI)
 void printField(char**, const Settings&, HBRUSH&, HBRUSH&,
 	const unsigned int&, HDC&);
-//Çàïîëíåíèå ïîëÿ ïîëüçîâàòåëåì
+//Заполнение поля пользователем
 void fillField(char**, const Settings&);
-//Çàïîëíåíèå ïîëÿ ñëó÷àéíûì îáðàçîì
+//Заполнение поля случайным образом
 void randomFillField(char**, const Settings&);
-//Ôóíêöèÿ, çàíèìàþùàÿñÿ îðãàíèçàöèåé èãðîâîãî ïðîöåññà
+//Функция, занимающаяся организацией игрового процесса
 void startGame(char**, const Settings&);
-//Ãåíåðàöèÿ ñëåäóþùåãî ïîêîëåíèÿ êëåòîê. Âîçâðàùàåò êîë-âî æèâûõ êëåòîê
+//Генерация следующего поколения клеток. Возвращает кол-во живых клеток
 unsigned long long nextGenField(char**, const Settings&);
-//Ôóíêöèÿ ïîäñ÷¸òà æèâûõ ñîñåäåé
+//Функция подсчёта живых соседей
 unsigned short int countNeighbours(char**, const int& line, const int& column);
 
 void main() {
@@ -47,34 +47,34 @@ void main() {
 
 	system("pause");
 	for (int i = 0; i < settings->fieldHeight; ++i) {
-		delete[] field[i];
+		deletefield[i];
 	}
 	delete field;
 	delete settings;
 }
 
 void primarySetup(Settings& settings) {
-	cout << "Ââåäèòå øèðèíó èãðîâîãî ïîëÿ: ";
+	cout << "Введите ширину игрового поля: ";
 	cin >> settings.fieldWidth;
 	while (settings.fieldWidth <= 0) {
-		cout << "Ââåäèòå çíà÷åíèå áîëüøå íóëÿ: ";
+		cout << "Введите значение больше нуля: ";
 		cin >> settings.fieldWidth;
 	}
-	settings.fieldWidth += 2; //Óâåëè÷èì øèðèíó ïîëå íà 2
+	settings.fieldWidth += 2; //Увеличим ширину поле на 2
 
-	cout << "Ââåäèòå âûñîòó èãðîâîãî ïîëÿ: ";
+	cout << "Введите высоту игрового поля: ";
 	cin >> settings.fieldHeight;
 	while (settings.fieldHeight <= 0) {
-		cout << "Ââåäèòå çíà÷åíèå áîëüøå íóëÿ: ";
+		cout << "Введите значение больше нуля: ";
 		cin >> settings.fieldHeight;
 	}
-	settings.fieldHeight += 2; //Óâåëè÷èì âûñîòó ïîëÿ íà 2
+	settings.fieldHeight += 2; //Увеличим высоту поля на 2
 
-	cout << "Ââåäèòå ìàêñèìàëüíîå ÷èñëî ãåíåðàöèé íîâûõ ïîêîëåíèé"
-		"(0 äëÿ îòìåíû îãðàíè÷åíèÿ): ";
+	cout << "Введите максимальное число генераций новых поколений"
+		"(0 для отмены ограничения): ";
 	cin >> settings.maxStep;
 
-	cout << "Ââåäèòå çàäåðæêó â ñåêóíäàõ ìåæäó îòðèñîâêîé ïîêîëåíèé: ";
+	cout << "Введите задержку в секундах между отрисовкой поколений: ";
 	cin >> settings.delay;
 	if (settings.delay < 0) {
 		settings.delay = 0.0;
@@ -84,17 +84,17 @@ void primarySetup(Settings& settings) {
 
 void printField(char** field, const Settings& settings, HBRUSH& Cells,
 	HBRUSH& Background, const unsigned int& cellSize, HDC& dc) {
-	//Ïåðåáèðàåì âñå ýëåìåíòû(Êðîìå êðàÿ ïîëÿ)
+	//Перебираем все элементы(Кроме края поля)
 	for (int line = 1; line < (settings.fieldHeight - 1); ++line) {
 		for (int column = 1; column < (settings.fieldWidth - 1); ++column) {
-			//Åñëè íàøëè æèâóþ êëåòêó, òî ðèñóåì å¸
+			//Если нашли живую клетку, то рисуем её
 			if (field[line][column] == CELL_SYMBOL) {
 				SelectObject(dc, Cells);
 				Ellipse(dc, column * cellSize - cellSize,
 					line * cellSize - cellSize + INDENT,
 					column * cellSize, line * cellSize + INDENT);
 			}
-			//Èíà÷å ðèñóåì ïóñòóþ êëåòêó
+			//Иначе рисуем пустую клетку
 			else {
 				SelectObject(dc, Background);
 				Ellipse(dc, column * cellSize - cellSize,
@@ -106,14 +106,14 @@ void printField(char** field, const Settings& settings, HBRUSH& Cells,
 }
 
 void fillField(char** field, const Settings& settings) {
-	//Áóôåðíàÿ ñòðîêà äëÿ çàïîëíåíèÿ ïîëÿ
+	//Буферная строка для заполнения поля
 	char* inputLine = new char[settings.fieldWidth - 1];
 
-	cout << "Ïðîèçâîäèòå çàïîëíåíèå ïî îäíîé ñòðîêå çà ðàç.\n"
-		<< "Âàì íåîáõîäèìî ââîäèòü ñòðîêè âèäà \"010010\",\n"
-		<< "ãäå 0 - ì¸ðòâàÿ êëåòêà, à 1 - æèâàÿ êëåòêà.\n";
+	cout << "Производите заполнение по одной строке за раз.\n"
+		<< "Вам необходимо вводить строки вида \"010010\",\n"
+		<< "где 0 - мёртвая клетка, а 1 - живая клетка.\n";
 	cin.ignore();
-	//Íà ìåñòàõ åäèíèö â ñòðîêå ñòàâÿòñÿ æèâûå êëåòêè
+	//На местах единиц в строке ставятся живые клетки
 	for (int line = 1; line < (settings.fieldHeight - 1); ++line) {
 		cin.getline(inputLine, settings.fieldWidth - 1);
 		for (int column = 1; column < (settings.fieldWidth - 1); ++column) {
@@ -147,9 +147,9 @@ void startGame(char** field, const Settings& settings) {
 	unsigned long long  liveCells;
 	unsigned short      cellSize;
 	COORD               coords;
-	char*               answer = new char;
+	char* answer = new char;
 
-	cout << "Çàïîëíèòü ïîëå âðó÷íóþ?(Y/N): ";
+	cout << "Заполнить поле вручную?(Y/N): ";
 	cin >> *answer;
 	if ('Y' == *answer || 'y' == *answer) {
 		fillField(field, settings);
@@ -160,8 +160,8 @@ void startGame(char** field, const Settings& settings) {
 
 	delete answer;
 
-	cout << "Ââåäèòå ðàçìåð êëåòêè(Öåëîå ÷èñëî áîëüøå 3. "
-		"Ðåêîìåíäóåìûé ðàçìåð = 8): ";
+	cout << "Введите размер клетки(Целое число больше 3. "
+		"Рекомендуемый размер = 8): ";
 	cin >> cellSize;
 
 	HANDLE hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -169,11 +169,11 @@ void startGame(char** field, const Settings& settings) {
 
 	system("cls");
 	SetConsoleTextAttribute(hConsoleHandle, 0x06);
-	cout << "Ðàçìåð ïîëÿ: " << settings.fieldWidth - 2
-		<< "x" << settings.fieldHeight - 2 << ", çàäåðæêà: "
-		<< settings.delay / 1000 << ", ìàêñèìóì èòåðàöèé: "
+	cout << "Размер поля: " << settings.fieldWidth - 2
+		<< "x" << settings.fieldHeight - 2 << ", задержка: "
+		<< settings.delay / 1000 << ", максимум итераций: "
 		<< settings.maxStep << endl;
-	cout << "Äëÿ çàêðûòèÿ ïðîãðàììû èñïîëüçóéòå ñî÷åòàíèå êëàâèø Alt + F4.\n";
+	cout << "Для закрытия программы используйте сочетание клавиш Alt + F4.\n";
 
 	HWND    hwnd = GetConsoleWindow();
 	HDC     dc = GetDC(hwnd);
@@ -193,7 +193,7 @@ void startGame(char** field, const Settings& settings) {
 		Sleep(settings.delay);
 		liveCells = nextGenField(field, settings);
 		SetConsoleCursorPosition(hConsoleHandle, coords);
-		cout << "Êîë-âî æèâûõ êëåòîê: " << liveCells << "    ";
+		cout << "Кол-во живых клеток: " << liveCells << "    ";
 		printField(field, settings, Cells, Background, cellSize, dc);
 		++(*currStep);
 	} while (settings.maxStep != *currStep && liveCells != 0);
@@ -209,22 +209,22 @@ void startGame(char** field, const Settings& settings) {
 unsigned long long nextGenField(char** field, const Settings& settings) {
 	unsigned short int* cellMates = new unsigned short int;
 	unsigned long long  count = 0;
-	//Èùåì ìåñòà äëÿ çàðîæäåíèÿ è óíè÷òîæåíèÿ êëåòîê
+	//Ищем места для зарождения и уничтожения клеток
 	for (int line = 1; line < (settings.fieldHeight - 1); ++line) {
 		for (int column = 1; column < (settings.fieldWidth - 1); ++column) {
 			*cellMates = countNeighbours(field, line, column);
-			//Ìåðòâà è 3 ñîñåäà - îæèâèòü
+			//Мертва и 3 соседа - оживить
 			if (' ' == field[line][column] && 3 == *cellMates) {
 				field[line][column] = 'Ð';
 			}
-			//Æèâà è íå 2 è íå 3 ñîñåäà - óáèòü
+			//Жива и не 2 и не 3 соседа - убить
 			else if (CELL_SYMBOL == field[line][column] &&
 				(2 != *cellMates && 3 != *cellMates)) {
 				field[line][column] = 'Ì';
 			}
 		}
 	}
-	//Âûðàùèâàåì ñëåäóþùåå ïîêîëåíåå
+	//Выращиваем следующее поколение
 	for (int line = 1; line < (settings.fieldHeight - 1); ++line) {
 		for (int column = 1; column < (settings.fieldWidth - 1); ++column) {
 			if (' ' != field[line][column]) {
